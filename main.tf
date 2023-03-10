bb274952a9788614c5f8f1b2e7e63d248e5006fb
Valande <valande@gmail.com>
2023-03-10 21:41:37 +0100

    aws_s3_bucket_website_configuration

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ">= 4.56.0"
+      version = ">= 4.58.0"
     }
   }
 }
@@ -36,10 +36,32 @@ resource "aws_s3_bucket_policy" "valbucket-valande-policy" {
   })
 }
 
+resource "aws_s3_bucket_website_configuration" "valbucket-valande-website" {
+  bucket = aws_s3_bucket.valbucket-valande.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+  routing_rule {
+    condition {
+      http_error_code_returned_equals = 404
+    }
+    redirect {
+      replace_key_with = "error.html"
+    }
+  }
+}
+
 resource "aws_s3_object" "index" {
   bucket = aws_s3_bucket.valbucket-valande.bucket
   key    = "index.html"
   source = "htdocs/index.html"
+  source_hash = filemd5("htdocs/index.html")
   content_type = "text/html"
 }
 
@@ -47,9 +69,10 @@ resource "aws_s3_object" "error" {
   bucket = aws_s3_bucket.valbucket-valande.bucket
   key    = "error.html"
   source = "htdocs/error.html"
+  source_hash = filemd5("htdocs/error.html")
   content_type = "text/html"
 }
 
 output "s3_endpoint" {
-  value = aws_s3_bucket.valbucket-valande.bucket_domain_name
+  value = "${aws_s3_bucket.valbucket-valande.bucket_domain_name}/index.html"
 }
